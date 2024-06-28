b05548a9c8d21dc421f4977f3ba850f533ce605c
Bilbo 1Gaming <bilbo1gaming@gmail.com>
2024-06-29 00:20:24 +0100

    Complete Ticket search & User Search & DB Testing
    Co-authored-by: Noah <ImNoahDev@users.noreply.github.com>

--- bun/src/index.ts
+++ bun/src/index.ts
@@ -1,9 +1,9 @@
 import { Database } from "bun:sqlite";
 import Logger from "louis-log"
-
 import schedule from "node-schedule"
 
 import { getUserInfo, getAllChannelMembers } from "./utils/getUsers"
+import { getStatsForUser } from "./utils/getHack";
 
 const logger = new Logger("hackhour-leaderboard","index",{
     logWebook:  {
@@ -15,8 +15,11 @@ const logger = new Logger("hackhour-leaderboard","index",{
 )
 logger.success("Entered Index")
 
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
 const db = new Database("db.sqlite",{create: true});
+db.exec('PRAGMA foreign_keys = ON')
+
 
 // db.run(`CREATE TABLE IF NOT EXISTS 'users_tmp' ('id' TEXT PRIMARY KEY NOT NULL, 'username' TEXT, 'displayName' TEXT);`);
 // db.run(`CREATE TABLE IF NOT EXISTS 'tickets_tmp' ('id' INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, 'user' TEXT NOT NULL REFERENCES 'users'('id'), 'unix' INTEGER NOT NULL, 'sessions' INTEGER NOT NULL, 'minutes' INTEGER NOT NULL);
@@ -24,12 +27,36 @@ const db = new Database("db.sqlite",{create: true});
 
 logger.debug(db.query("select 'hello world' as message").get())
 
-schedule.scheduleJob('*/10 * * * * *', updateTicketCount );
-schedule.scheduleJob('*/20 * * * * *', updateUserList );
+// schedule.scheduleJob('*/10 * * * * *', updateTicketCount );
+// schedule.scheduleJob('*/20 * * * * *', updateUserList );
 
-  function updateTicketCount(){
+updateTicketCount()
+
+  async function updateTicketCount(){
+    const currentTime = Math.floor(new Date().getTime() / 1000)
+
     logger.log("Starting Ticket Count Update Process")
-  }
+
+    const members = db.query("SELECT id FROM users").all() 
+    // logger.debug("",members)
+    logger.debug("Member Count from DB:", members.length)
+
+    for (const member in members) {
+        await sleep(20)
+        let stats = await getStatsForUser(members[member].id)
+        logger.debug(stats)
+        if (stats == null) continue;
+        if (stats.sessions == 0 || stats.total == null || stats.total == 0) continue;
+        logger.debug("Saving to DB",{member: members[member].id, stats})
+        logger.success(db.query("INSERT INTO tickets ( id, user , unix , sessions, minutes ) VALUES (null, $user, $unix, $sessions, $minutes)").run({
+            $user: members[member].id,
+            $unix: currentTime,
+            $sessions: stats.sessions,
+            $minutes: stats.total
+        }))
+    }
+}
+  
 async function updateUserList(){
     logger.log("Starting Update User List Process")
 
--- bun/src/utils/getHack.ts
+++ bun/src/utils/getHack.ts
@@ -10,39 +10,32 @@ const logger = new Logger("hackhour-leaderboard", "getHack", {
     }
 });
 
-
-
 //Function to fetch number of sessions
-async function getStatsForUser(slackId: string): Promise<number> {
+export async function getStatsForUser(slackId: string): Promise<object | null> {
     const url = `https://hackhour.hackclub.com/api/stats/${slackId}`;
     
     try {
-        logger.debug("1")
+        logger.debug(url)
         const response = await fetch(url);
-        logger.debug("2")
         if (!response.ok) {
-            logger.debug("3")
+            
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
-        logger.debug("5")
         const data = await response.json();
-        logger.debug("6",data)
         if (data.data) {
-            logger.debug("7")
             return data.data;
         } else {
-            logger.debug("8")
             throw new Error('Failed to retrieve sessions');
         }
     } catch (error) {
         logger.error('Error fetching sessions:', error);
-        throw error;
+        return null
     }
 }
 
 // Example usage
 async function main() {
-    const userId = "U0735FTMS3V"; // Get user id from command line argument
+    const userId = ""; // Get user id from command line argument
     if (!userId) {
         logger.error('Please provide a user id as an argument.');
         return;
